92cd743cfaffc58803425559a578e781f8d35037
Jackson Linus <jacksonlinus95@gmail.com>
2020-12-03 18:31:10 +0300

    Function to generate grid ID automatically
    generate unique id using python date time instead of UUID, the function rundown to milliseconds so as to make the numbers unique.

---
+++ GridIDAutoGenerate.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import time
+
+
+def grid_id():
+    prefix = "PGRD"
+    now = datetime.now()
+    data_time = str(int(now.strftime("%Y%m%d%H%M%S%f")))
+    uid = prefix+data_time
+    print(uid)
+    return(uid)
+grid_id()
+
